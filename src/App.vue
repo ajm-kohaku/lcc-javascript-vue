95361d1a2fd05cb3f55f3649e586c2ec45c0f796
Amber Murphy <45693571+ajm-kohaku@users.noreply.github.com>
2021-01-14 22:03:46 -0500

    setup vue project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,400&family=Raleway:ital,wght@0,400;0,500;1,500&display=swap");
+$primary: #8132a8;
+$font-family: "Raleway", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial,
+  sans-serif;
+@import "~bootstrap/scss/bootstrap";
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="alert alert-primary" role="alert">
+      This is a primary alert—check it out!
+    </div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
