e85180b53a0658a714ac10b5d3f9bf441ed25239
Amber Murphy <45693571+ajm-kohaku@users.noreply.github.com>
2021-01-16 00:33:32 -0500

    making updates to deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,16 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
- echo 'kohaku.me' > CNAME
+#  echo 'kohaku.me' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:ajm-kohaku/lcc-javascript-vue.git master
+# git push -f git@github.com:ajm-kohaku/lcc-javascript-vue.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:ajm-kohaku/lcc-javascript-vue.git master:gh-pages
 
 cd -
