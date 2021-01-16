0d989f9a8133bba83abc2302d3aa670908b469d4
Amber Murphy <45693571+ajm-kohaku@users.noreply.github.com>
2021-01-16 00:09:36 -0500

    changing push branch

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:ajm-kohaku/lcc-javascript-vue.git main
+git push -f git@github.com:ajm-kohaku/lcc-javascript-vue.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
