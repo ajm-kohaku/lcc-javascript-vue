0916284b926d92ab16e9a32d5fb4291c9f955200
Amber Murphy <45693571+ajm-kohaku@users.noreply.github.com>
2021-01-15 21:30:45 -0500

    prepping assignment 7

--- src/views/Assignment7.vue
+++ src/views/Assignment7.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>
-    
+  <div id="list-container">
+    <h1 class="list-header"></h1>
+    <h2 id="list-sub-header">Game Systems Played</h2>
+    <form action="fake-success.html">
+      <div class="label-input">
+        <label for="systemInput">Game System</label>
+        <input type="text" id="systemInput" />
+      </div>
+      <div id="buttons">
+        <a data-state="record" href="">record</a>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Assignment3'
-}
+  name: "Assignment7",
+};
 </script>
--- src/views/Assignment8.vue
+++ src/views/Assignment8.vue
@@ -6,6 +6,6 @@
 
 <script>
 export default {
-  name: 'Assignment3'
+  name: 'Assignment8'
 }
 </script>
