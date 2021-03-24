7bc0ed9b1b7ef29adca1042c2bdaaea6bdd21841
Amber Murphy <45693571+ajm-kohaku@users.noreply.github.com>
2021-03-23 22:05:22 -0400

    scoping css

--- src/components/Assignment5.vue
+++ src/components/Assignment5.vue
@@ -84,7 +84,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 label {
   color: black !important;
 }
--- src/components/Assignment8.vue
+++ src/components/Assignment8.vue
@@ -73,7 +73,7 @@ export default {
 };
 </script> 
 
-<style lang="scss">
+<style scoped lang="scss">
 @import "@/assets/list-king.scss";
 .custom-list {
   padding: 0;
