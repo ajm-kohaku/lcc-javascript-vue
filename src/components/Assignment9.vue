d803cf98b70630c47f807f0eef934b9d06dfe280
Amber Murphy <45693571+ajm-kohaku@users.noreply.github.com>
2021-04-08 19:06:01 -0400

    update formatting on Assignment9 file

--- src/components/Assignment9.vue
+++ src/components/Assignment9.vue
@@ -7,9 +7,9 @@
 </template>
 
 <script>
-import Accordion from "./Assignment9Accordion.vue";
+import Accordion from './Assignment9Accordion.vue'
 export default {
-  name: "Assignment9",
+  name: 'Assignment9',
   components: {
     Accordion,
   },
@@ -18,26 +18,29 @@ export default {
       accordionData: [
         {
           id: 1,
-          header: "Accordion Example",
+          header: 'Accordion Example',
           message:
-            "This page represents an example of accordion design. It is very simple and can help save screen real estate.",
+            'This page represents an example of accordion design. It is very simple and can help save screen real estate.',
+          comp: '',
         },
         {
           id: 2,
-          header: "Local Storage Example",
+          header: 'Local Storage Example',
           message:
             'The next mini exercise is to emulate <em><a href="assgmt-9-local-storage">local storage</a></em>. In practice, however, we just submit a form and simulate a response.',
+          comp: '',
         },
         {
           id: 3,
-          header: "Photo Viewer Example",
+          header: 'Photo Viewer Example',
           message:
-            "The next mini exercise is to demonstrate a <em><a href=\"assgmt-9-photo-viewer\">photo viewer</a></em>. Please go to the photo viewer example to view it.",
+            'The next mini exercise is to demonstrate a <em><a href="assgmt-9-photo-viewer">photo viewer</a></em>. Please go to the photo viewer example to view it.',
+          comp: '',
         },
       ],
-    };
+    }
   },
-};
+}
 </script> 
 
 <style lang="scss" scoped>
